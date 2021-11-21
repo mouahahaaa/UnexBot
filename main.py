17c43204875b31894e9b7690dc1c04e723a833ef
MOUAHAHAAA <86555955+mouahahaaa@users.noreply.github.com>
2021-11-21 01:52:13 +0100

    Base perco_program

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ default_intents.members = True
 
 client = discord.Client(intents=default_intents)
 
-fiveMatrixPoints = np.zeros((5,2))
+fiveListPlayers = np.zeros((5, 1), dtype=object)
 fiveTeamSize = 5
 fiveBufferCommandPlayers = 3
+fiveWin = True
+fiveAtk = True
+fiveBaremeWinAtk = 3 #points gdoc
+fiveBaremeLoseAtk = 1 #points gdoc
+fiveBaremeWinDef = 2 #points gdoc
+fiveBaremeLoseDef = 1 #points gdoc
+fivePlayerPoints = 0
 
 @client.event
 async def on_ready():
@@ -21,21 +28,35 @@ async def on_message(message):
 
         if message.content.split()[1] == "-w":
             await message.channel.send("C'est une win")
+            fiveWin = True
         elif message.content.split()[1] == "-l":
             await message.channel.send("C'est une lose")
+            fiveWin = False
 
 #######################################################################################
 
         if message.content.split()[2] == "-a":
             await message.channel.send("En attaque")
+            fiveAtk = True
         elif message.content.split()[2] == "-d":
             await message.channel.send("En défense")
+            fiveAtk = Lose
 
 #######################################################################################
 
-        for i in range(4):
-            fiveMatrixPoints[i,1] = message.content.split()[fiveBufferCommandPlayers + i]
-
+        for i in range(fiveTeamSize):
+            fiveListPlayers[i, 0] = message.content.split()[fiveBufferCommandPlayers + i]
+            if fiveAtk == True:
+                if fiveWin == True:
+                    fivePlayerPoints = fiveBaremeWinAtk
+                elif fiveWin == False:
+                    fivePlayerPoints = fiveBaremeLoseAtk
+            elif fiveAtk == False:
+                if fiveWin == True:
+                    fivePlayerPoints = fiveBaremeWinDef
+                elif fiveWin == False:
+                    fivePlayerPoints = fiveBaremeLoseDef
+        print(fiveListPlayers)
 
 
 @client.event
